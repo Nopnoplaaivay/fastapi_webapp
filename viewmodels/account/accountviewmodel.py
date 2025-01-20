fd1d4b6e604afb6871505739cf413d0e9a8ef81f
KhangVinh <vinhkhang378@gmail.com>
2025-01-20 11:36:14 +0700

    register

--- viewmodels/account/accountviewmodel.py
+++ viewmodels/account/accountviewmodel.py
@@ -1,4 +1,9 @@
+from starlette.requests import Request
+
+from data.user import User
 from viewmodels.shared.viewmodel import ViewModelBase
 
 class AccountViewModel(ViewModelBase):
-    pass
+    def __init__(self, request: Request):
+        super().__init__(request)
+        self.user = User(name="Michael", email="michael6104@gmail.com", hash_password="123456")
--- views/account.py
+++ views/account.py
@@ -1,5 +1,6 @@
 import fastapi
 from fastapi.requests import Request
+from fastapi_chameleon import template
 
 from viewmodels.account.accountviewmodel import AccountViewModel
 from viewmodels.account.loginviewmodel import LoginViewModel
@@ -10,22 +11,34 @@ router = fastapi.APIRouter()
 
 
 @router.get('/account')
+@template()
 def index(request: Request):
     vm = AccountViewModel(request)
     return vm.to_dict()
 
 
 @router.get('/account/register')
+@template()
 def register(request: Request):
+    print("GET REGISTER")
+    vm = RegisterViewModel(request)
+    return vm.to_dict()
+    
+@router.post('/account/register')
+@template()
+def register(request: Request):
+    print("POST REGISTER")
     vm = RegisterViewModel(request)
     return vm.to_dict()
 
 @router.get('/account/login')
+@template()
 def login(request: Request):
     vm = LoginViewModel(request)
     return vm.to_dict()
     
 
 @router.get('/account/logout')
+@template()
 def logout(request: Request):
     return {}
